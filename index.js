cdfec5922b186585497e996805c8731ed19e18a9
Nolan Rummel <nolan.rummel22@gmail.com>
2023-05-25 13:10:18 -0400

    worked with robert to condense original functions and added keystroke eventListener

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
+    // const input = document.getElementById('button');
 
-    function clickAlert() {
-        alert('I was clicked!');
-    }
+    // const clickAlert = () => alert('I was clicked!');
 
-    input.addEventListener('click', clickAlert);
+    document.getElementById('button').addEventListener('click', clickAlert = () => alert('I was clicked!'))
+
+    // input.addEventListener('click', clickAlert);
 }
+
+// function newEventListener() {
+//     // document.addEventListener('keydown', clickAlert = () => alert('I was pressed!'))
+//     // document.getElementById('button2').addEventListener('onload', keyAlert = () => alert('I was pressed!'))
+//     // document.keydown = () => alert('I was pressed!')
+//     document.addEventListener('keydown', k => {
+//         // console.log(k.target)
+//         if (k.key === "ArrowLeft"){
+//             alert('did this work when we pressed?')
+//         }
+//         else {
+//             console.log('this didnt work')
+//         }
+//     })
+// }
+
+function arrowVersions() {
+    document.getElementById('button2').addEventListener() => k (k.key === "ArrowLeft") ? 'this worked' : 'this didnt work'
+}
+
+
+//btn2 = document.getElementById('button2')
+
+// function moveDodgerLeft() {
+//     let left = parseInt(btn2.style.left.replace("px", ""));
+//     console.log(left)
+//     if (left > 0) {
+//         alert('i was pressed')
+//     }
+// };
+
+addingEventListener()
+// newEventListener()
+arrowVersions()
